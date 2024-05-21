822a8d1182ab44ec06106a5b48c90cdb27125011
me.mickaelly <104102435+Mickaellysilva@users.noreply.github.com>
2024-05-21 08:43:19 -0300

    Create operadoresAritmeticos.js

---
+++ operadoresAritmeticos.js
@@ -0,0 +1,41 @@
+//operadores aritméticos
+x=4+1
+console.log(x)
+//potenciação
+y=4**5
+console.log(y)
+//módulo
+z=7%2
+console.log(z)
+//operações aritméticas com os códigos
+let numero = 14
+numero=14+x
+console.log(numero)
+//exercícios do curso
+primeiroNumero=133;
+segundoNumero=87;
+resultadoDaSubtracao=primeiroNumero-segundoNumero;
+console.log(resultadoDaSubtracao)
+
+
+dividendo=192;
+divisor=12;
+resultadoDaDivisao=dividendo/divisor;
+console.log(resultadoDaDivisao)
+
+
+let multiplicador = 133;
+let multiplicando = 13;
+let resultadoDaMultiplicacao = multiplicador*multiplicando;
+console.log(resultadoDaMultiplicacao)
+
+
+base=2;
+expoente=10;
+potencia=base**expoente;
+console.log(potencia)
+
+dividendo=11;
+divisor=3;
+restoDaDivisao=dividendo % divisor
+console.log("O resto da divsão é"+ + restoDaDivisao)
