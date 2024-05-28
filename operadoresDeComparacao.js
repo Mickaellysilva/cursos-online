199abcd86a355d2e69c9229ffa9285e79412e54e
me.mickaelly <104102435+Mickaellysilva@users.noreply.github.com>
2024-05-28 09:29:45 -0300

    Create operadoresDeComparacao.js

---
+++ operadoresDeComparacao.js
@@ -0,0 +1,69 @@
+/*Exercício 1 - Crie e compare variáveis: É menor, maior ou igual?
+Imagine que você precisa comprar duas peças de roupa, entretanto, após o pagamento de suas contas, você tem somente R$ 60,00 reais para gastar. Faça um algoritmo para verificar se as duas peças de que precisa, caberão no seu orçamento.
+Crie duas variáveis roupa1 e roupa2:
+Na variável chamada de roupa1, você vai adicionar o valor 35;
+Na variável chamada de roupa2, você vai adicionar o valor 20;
+Crie a variável compras:
+Atribua a ela o valor da soma entre roupa1 e roupa2>;
+Crie uma variável chamada orcamento:
+Verifique se compras é menor ou igual a 60 e atribua o retorno a variável orcamento.
+*/
+
+roupa1=35;
+roupa2=20;
+compras= roupa1 + roupa2
+orcamento = compras <= 60;
+
+/* Exercício 2 - Comparando as idades
+Nesse exercício você vai desenvolver um programa capaz de verificar duas idades.
+
+Crie as variáveis idadeDaThays e idadeDoDouglas:
+Crie uma variável chamada idadeDaThays e atribua o valor de 53;
+Crie uma variável chamada idadeDoDouglas e atribua o valor de 49;
+Compare as idades, armazenando o resultado das comparações:
+Siga o padrão para o nome das variáveis que armazenarão as comparações comparacaoN, onde N é o número da comparação.
+comparacao1: Compare se idadeDaThays é maior que idadeDoDouglas;
+comparacao2: Compare se idadeDoDouglas é menor que idadeDaThays.
+*/
+idadeDaThays=53;
+idadeDoDouglas=49;
+comparacao1=idadeDaThays>idadeDoDouglas;
+comparacao2=idadeDoDouglas<idadeDaThays;
+comparacaoN=2;
+
+
+// outras questões
+let sapato = 15;
+let chinelo = 22;
+let pantufa = 33;
+/***** Não altere os códigos dentro desse bloco *****/
+/***** Escreva seu código nas linhas abaixo *****/
+verificacao1=sapato>=15;
+verificacao2=chinelo>=12;
+verificacao3=pantufa<=18;
+console.log(verificacao1);
+console.log(verificacao2);
+console.log(verificacao3);
+
+/***** Não altere os códigos dentro desse bloco *****/
+let senhaDoWiFi = 159000;
+
+let senhaDigitada1 = 169000;
+let senhaDigitada2 = 159000;
+let senhaDigitada3 = '159000';
+let senhaDigitada4 = 159000;
+/***** Não altere os códigos dentro desse bloco *****/
+
+/***** Escreva seu código nas linhas abaixo *****/
+
+senhaDigitadaN=senhaDoWiFi;
+verificacao1=senhaDigitada1===senhaDoWiFi;
+verificacao2=senhaDigitada2===senhaDoWiFi;
+verificacao3=senhaDigitada3===senhaDoWiFi;
+verificacao4=senhaDigitada4===senhaDoWiFi;
+verificacaoN=4;
+
+
+
+
+
