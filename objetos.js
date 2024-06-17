fd38d53c6654dfd3851841f375640666ecd1c359
me.mickaelly <104102435+Mickaellysilva@users.noreply.github.com>
2024-06-17 15:13:50 -0300

    Create objetos.js

---
+++ objetos.js
@@ -0,0 +1,14 @@
+/*
+forma tradicional 
+let nome="MICKA";
+let idade=17;
+let sexo='fem';
+*/
+//objeto= juntar tudo e criar um todo
+let usuarioUm={
+    nome:"MICKA",
+    idade:17,
+    sexo:'fem'
+}
+
+console.log(usuarioUm)
