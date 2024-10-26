c1a2b391e2dcf326406b4d4dca5bc1901b7ef752
me.mickaelly <104102435+Mickaellysilva@users.noreply.github.com>
2024-10-26 13:21:35 -0300

    Create Variaveis.C

---
+++ Variaveis.C
@@ -0,0 +1,10 @@
+/* Crie variáveis para armazenar seu nome, idade e altura. Em seguida, imprima esses valores.*/
+#include <stdio.h>
+int main() {
+char nome[]="Mickaelly";
+int idade = 18;
+float altura = 1.59;
+printf("Olá meu nome é  %s\n",nome);
+printf("minha altura é %.2f\n",altura);
+printf("e minha idade é %d\n",idade);
+}
